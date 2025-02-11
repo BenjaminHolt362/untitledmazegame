ce29c00ef4d39740498bb947e40fd61b12b7ce0a
givensuman <givensuman@duck.com>
2025-02-11 09:29:45 -0500

    add gun support, need to modularize gun components

--- pistol/pistol.gd
+++ pistol/pistol.gd
@@ -9,23 +9,42 @@ func _ready():
 
 func shoot() -> void:
 	const BULLET = preload("res://bullet/bullet.tscn")
+	
+	if _gun == GUNS.shotgun:
+		for rotation in range(-3, 3):
+			var _new_bullet = BULLET.instantiate()
+			_new_bullet.global_position = %ShootingPoint.global_position
+			_new_bullet.global_rotation = %ShootingPoint.global_rotation + (rotation * 0.2)
+			%ShootingPoint.add_child(_new_bullet)
+		
+		return
+	
 	var _new_bullet = BULLET.instantiate()
 	_new_bullet.global_position = %ShootingPoint.global_position
-	_new_bullet.global_rotation = %ShootingPoint.global_rotation
+	
+	if _gun == GUNS.ak47:
+		_new_bullet.global_rotation = %ShootingPoint.global_rotation + (randf())
+	else:
+		_new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(_new_bullet)
 
-enum GUNS { shotgun, ak47 }
+enum GUNS { shotgun, ak47, pistol }
+var _gun = GUNS.pistol
+
 func change_gun(gun: GUNS) -> void:
 	if gun == GUNS.shotgun:
+		_gun = gun
 		var s = load("res://pistol/shotgun.png")
 		%Gun.texture = s
 		
 	elif gun == GUNS.ak47:
+		_gun = gun
 		var s = load("res://pistol/ak47.png")
 		%Gun.texture = s
 		
 		$Timer.wait_time = 0.1
 	else:
+		_gun = GUNS.pistol
 		var s = load("res://pistol/pistol.png")
 		%Gun.texture = s
 
